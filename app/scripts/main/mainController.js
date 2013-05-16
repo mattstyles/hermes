40811d80246acf2c3327be440ec7063527673090
Matt Styles <matt@veryfizzyjelly.com>
2013-05-16 21:00:35 +0100

    Added dummy data from the message controller
    Model is part of the controller

--- app/scripts/main/mainController.js
+++ app/scripts/main/mainController.js
@@ -1,68 +1,12 @@
 'use strict';
 
-angular.module( 'yoAngularExpressTestApp' )
-    .controller( 'MainCtrl', [ '$scope', 'socket', '$location', '$window', function( $scope, socket, $location, $window ) {
+angular.module( 'hermesApp' )
+    .controller( 'MainCtrl', [ '$scope', 'socket', function( $scope, socket ) {
 
-        $scope.awesomeClientThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma',
-            'jQuery',
-            'UI-Bootstrap',
-            'Font-Awesome',
-            'Modernizr',
-            'lodash',
+        $scope.messages = [
+            { name: 'Albert', msg: 'How are you?' },
+            { name: 'Bernhard', msg: 'This is fun' },
+            { name: 'Cecil', msg: 'Angularise yo\' bad self' }
         ];
 
-        $scope.awesomeServerThings = [
-            'Express',
-            'Hogan',
-            'Socket.io',
-            'MongoDB'
-        ];
-
-        $scope.isCollapsed = true;
-
-        $scope.collapseText = 'Open';
-
-        $scope.collapseIcon = 'icon-chevron-down';
-
-        $scope.collapse = function() {
-            $scope.isCollapsed = !$scope.isCollapsed;
-
-            if ($scope.isCollapsed) {
-                $scope.collapseText = 'Open';
-                $scope.collapseIcon = 'icon-chevron-down';
-            } else {
-                $scope.collapseText = 'Close';
-                $scope.collapseIcon = 'icon-chevron-up';
-            }
-        };
-
-        $scope.connectedClass = 'icon-remove';
-        $scope.connected = 'Not Connected to Server';
-
-        socket.on( 'send:onConnect', function( data ) {
-            $scope.connected = data.data;
-            $scope.connectedClass = 'icon-link';
-        } );
-
-        $scope.socketExample = function() {
-            console.log('sending event to socket');
-            socket.emit( 'send:example', {
-                data: 'example'
-            } );
-        };
-
-        socket.on( 'send:example', function( data ) {
-            console.log('client socket on');
-
-            $('.hero-unit').css( 'background-color', '#DC002A' );
-        } );
-
-        $scope.redirect = function( path ) {
-            console.log('attempting to redirect to ' + path );
-//            $location.path( path );
-            $window.location.href = path;
-        };
     }]);
